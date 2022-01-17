2460a52c87e24d613cdd43c414a0f6a10ac47697
ianmajkut <ianedwinmajkut@gmail.com>
2022-01-16 22:10:44 -0300

    feat: crear diseño pantala login/registro junto con validaciones
    Utilizando el ReactiveFormsModule se implemento una validacion a los campos y para el email se utilizo un pattern de regex para validar el mismo

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+
+  miFormulario: FormGroup = this.fb.group({
+    email: ['test1@gmail.com', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.miFormulario.value)
+    console.log(this.miFormulario.valid)
   }
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['Test1', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  registrar(){
+    console.log(this.miFormulario.value)
+    console.log(this.miFormulario.valid)
   }
 
 }
