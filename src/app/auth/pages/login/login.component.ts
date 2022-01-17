4eed6104bd0a83705405bebcbbcf6cc5ba3bbc80
ianmajkut <ianedwinmajkut@gmail.com>
2022-01-17 10:45:53 -0300

    feat: crear servicio para probar peticion login a la API
    En enviroments se definio el baseUrl que utilizaremos para realizar las peticiones. En la carpeta services se definio un servicio para realizar las peticion post a la API pasandole el mail y la contraseña

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,19 +14,24 @@ export class LoginComponent implements OnInit {
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
 
   miFormulario: FormGroup = this.fb.group({
-    email: ['test1@gmail.com', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
+    email: ['test2@test.com', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
     password: ['123456', [Validators.required, Validators.minLength(6)]]
   })
 
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   login(){
     console.log(this.miFormulario.value)
-    
-    this.router.navigateByUrl('/dashboard')
+    const {email, password} = this.miFormulario.value
+
+    this.authService.login(email, password)
+        .subscribe(resp =>{
+          console.log(resp)
+        })
+    // this.router.navigateByUrl('/dashboard')
   }
 
 }
